eec219e3d5b8a5de26f8846b41d3485ac95c653c
pankeny <konrad_malocha@hotmail.com>
2018-11-22 17:23:25 +0100

    get all html of all games

--- src/main/java/com/tmw/etl/etlapp/processes/DataExtractor.java
+++ src/main/java/com/tmw/etl/etlapp/processes/DataExtractor.java
@@ -2,6 +2,8 @@ package com.tmw.etl.etlapp.processes;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,25 +26,43 @@ public class DataExtractor implements Callable<ArrayList<Document>> {
 
     private ArrayList<Document> extractData() {
         ArrayList<Document> docsByPages = new ArrayList<>();
+        ArrayList<Document> docsByGames = new ArrayList<>();
 
         try {
             for (int i = 1; ; i += 60) {
+
                 Document doc = Jsoup.connect("https://www.empik.com/multimedia/xbox-one/gry/,342402,s," + i + "?resultsPP=60").get();
                 doc.charset(Charset.forName("UTF-8"));
 
                 boolean hasElements = !(doc.getAllElements().hasClass("sort notFound"));
 
-                if (hasElements)
+                if (hasElements) {
                     docsByPages.add(doc);
-                else
+
+                    Elements gamesURL = doc.getElementsByClass("img seoImage");
+
+                    for (Element game : gamesURL) {
+                        Document gameDoc = Jsoup.connect("https://www.empik.com" + game.attr("href")).get();
+                        gameDoc.charset(Charset.forName("UTF-8"));
+
+                        docsByGames.add(gameDoc);
+
+                    }
+
+                }else{
                     break;
+                }
+
+
+
+
 
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        logger.info("" + docsByPages.size()); // CHECK IF THERE 7 PAGES
+        logger.info("Extracted " + docsByPages.size() + " pages."); //
         return docsByPages;
     }
 }
