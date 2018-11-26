b93f45c2d35e2444871c632ac9b1feb3e2f1662a
brq0 <bartoszturek0@gmail.com>
2018-11-26 22:05:41 +0100

    Revert "Merge branch 'getting-game-desc' of C:\Users\konra\Desktop\etl-app with conflicts."
    This reverts commit 8ad3267

--- src/main/java/com/tmw/etl/etlapp/processes/DataTransformer.java
+++ src/main/java/com/tmw/etl/etlapp/processes/DataTransformer.java
@@ -9,9 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.Callable;
 
-public class DataTransformer implements Callable<ArrayList<GameDetails>> {
+public class DataTransformer implements Callable<ArrayList<Game>> {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     private ArrayList<Document> rawDataPages;
 
@@ -20,49 +21,117 @@ public class DataTransformer implements Callable<ArrayList<GameDetails>> {
     }
 
     @Override
-    public ArrayList<GameDetails> call() {
+    public ArrayList<Game> call() {
         return transformData(rawDataPages);
     }
 
-    private ArrayList<GameDetails> transformData(ArrayList<Document> rawDataPages) {
-
-        ArrayList<GameDetails> games = new ArrayList<>();
+    private ArrayList<Game> transformData(ArrayList<Document> rawDataPages) {
+        ArrayList<Game> games = new ArrayList<>();
 
         int position = 1;
 
         for (Document gamePage : rawDataPages) {
 
+            Game game = new Game();
             GameDetails gameDetails = new GameDetails();
 
-            String gameId = gamePage.getElementsByClass("js-reco-productlist").first().attr("page-product-id");
-            String gameDescription = gamePage.getElementsByClass("productDescription").text();
-            String gameTitle = gamePage.getElementsByAttributeValue("property", "og:title").first().attr("content");
-            String gameImgUrl = gamePage.getElementsByAttributeValue("property", "og:url").first().attr("content");
-            String gamePrice = gamePage.getElementsByClass("ta-price").first().text();
-            String gameCategory = gamePage.getElementsByClass("productsList__product swiper-slide ta-product js-reco-product").attr("data-product-category");
-            Elements elements = gamePage.getElementsByClass("productDataTable").first().getElementsByClass("row--text row--text  attributeName");
+            String gameId = getGameId(gamePage);
+            String gameDescription = getGameDescription(gamePage);
+            String gameName = getGameName(gamePage);
+            String gameImgUrl = getGameImgUrl(gamePage);
+            String gamePrice = getGamePrice(gamePage);
+            String category = getGameCategory(gamePage);
 
-            String gameProducent = elements.get(1).text();
-            String gamePublisher = elements.get(2).text();
-            String gameDistributor = elements.get(3).text();
-            String releaseDate = elements.get(4).text();
+
+            Elements elements = getDetailsTableElements(gamePage);
+            String gameProducer = "";
+            String gamePublisher = "";
+            String gameReleaseDate = "";
+            String gamePegiUrl = "";
+
+            for (Element element : elements) {
+                String[] elemDt = element.text().split(":");
+
+                switch (elemDt[0]) {
+                    case "Wydawca":
+                        gamePublisher = getTableRowContent(elemDt);
+                        break;
+                    case "Data premiery":
+                        gameReleaseDate = getTableRowContent(elemDt);
+                        break;
+                    case "Producent":
+                        gameProducer = getTableRowContent(elemDt);
+                        break;
+                    case "Liczba nośników":
+
+                        break;
+                    case "Wersja językowa":
+
+                        break;
+                    case "PEGI":
+                        gamePegiUrl = getGamePegiUrl(element);
+                        break;
+                }
+            }
+
+            System.out.println(gameId + " -- " + gameImgUrl + " --- " + gameName + " --- " +  gamePrice + " --- " + category + " --- " + gamePegiUrl + " --- " + gameProducer + " ---- " + gamePublisher + " ---- " + gameReleaseDate);
+
+            game.setId(gameId);
+            game.setName(gameName);
 
             gameDetails.setId(gameId);
-            gameDetails.setName(gameTitle);
+            gameDetails.setName(gameName);
             gameDetails.setPosition(position);
             gameDetails.setPrice(gamePrice);
-            gameDetails.setCategory( gameCategory );
+            gameDetails.setCategory(category);
             gameDetails.setImgUrl(gameImgUrl);
-            gameDetails.setDescription(gameDescription);
-            gameDetails.setProducent(gameProducent);
-            gameDetails.setPublisher(gamePublisher);
-            gameDetails.setDistributor(gameDistributor);
-            gameDetails.setReleaseDate(releaseDate);
 
-            games.add(gameDetails);
-
+            games.add(game);
         }
 
         return games;
+    }
+
+    private String getGamePegiUrl(Element element) {
+        return "https://www.empik.com" + element.getElementsByClass("pegiCode").first()
+                .getElementsByTag("img").first().attr("src");
+    }
+
+    private Elements getDetailsTableElements(Document gamePage) {
+        return gamePage.getElementsByClass("productDataTable").first()
+                .getElementsByClass("row--text row--text  attributeName");
+    }
+
+    private String getGameCategory(Document gamePage) {
+        return gamePage.getElementsByAttributeValue("itemtype", "http://schema.org/BreadcrumbList").last()
+                .getElementsByAttributeValue("itemprop", "itemListElement").last().text();
+    }
+
+    private String getGamePrice(Document gamePage) {
+        return gamePage.getElementsByClass("ta-price").first().text();
+    }
+
+    private String getGameImgUrl(Document gamePage) {
+        return gamePage.getElementsByAttributeValue("property", "og:image").first().attr("content");
+    }
+
+    private String getGameName(Document gamePage) {
+        return gamePage.getElementsByAttributeValue("property", "og:title").first().attr("content");
+    }
+
+    private String getGameDescription(Document gamePage) {
+        return gamePage.getElementsByClass("productDescription").text();
+    }
+
+    private String getGameId(Document gamePage) {
+        return gamePage.getElementsByClass("js-reco-productlist").first().attr("page-product-id");
+    }
+
+    private String getTableRowContent(String[] elemDt) {
+        return String.join("", Arrays.copyOfRange(elemDt, 1, elemDt.length));
+    }
+
+    private String getPureTextOfTableRow(String row, String text) {
+        return row.substring(row.indexOf(text) + text.length()).trim();
     }
 }
