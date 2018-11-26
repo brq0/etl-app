8ad3267d28da1577646a954b38bac02c95672250
pankeny <konrad_malocha@hotmail.com>
2018-11-26 19:54:22 +0100

    Merge branch 'getting-game-desc' of C:\Users\konra\Desktop\etl-app with conflicts.

--- src/main/java/com/tmw/etl/etlapp/processes/DataTransformer.java
+++ src/main/java/com/tmw/etl/etlapp/processes/DataTransformer.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.concurrent.Callable;
 
 public class DataTransformer implements Callable<ArrayList<Game>> {
@@ -35,103 +34,52 @@ public class DataTransformer implements Callable<ArrayList<Game>> {
             Game game = new Game();
             GameDetails gameDetails = new GameDetails();
 
-            String gameId = getGameId(gamePage);
-            String gameDescription = getGameDescription(gamePage);
-            String gameName = getGameName(gamePage);
-            String gameImgUrl = getGameImgUrl(gamePage);
-            String gamePrice = getGamePrice(gamePage);
-            String category = getGameCategory(gamePage);
+            String gameId = gamePage.getElementsByClass("js-reco-productlist").first().attr("page-product-id");
+            String gameDescription = gamePage.getElementsByClass("productDescription").text();
+            String gameTitle = gamePage.getElementsByAttributeValue("property", "og:title").first().attr("content");
+            String gameImgUrl = gamePage.getElementsByAttributeValue("property", "og:url").first().attr("content");
+            String gamePrice = gamePage.getElementsByClass("ta-price").first().text();
 
 
-            Elements elements = getDetailsTableElements(gamePage);
-            String gameProducer = "";
-            String gamePublisher = "";
-            String gameReleaseDate = "";
-            String gamePegiUrl = "";
 
-            for (Element element : elements) {
-                String[] elemDt = element.text().split(":");
+            Elements elements = gamePage.getElementsByClass("productDataTable").first().getElementsByClass("row--text row--text  attributeName");
 
-                switch (elemDt[0]) {
-                    case "Wydawca":
-                        gamePublisher = getTableRowContent(elemDt);
-                        break;
-                    case "Data premiery":
-                        gameReleaseDate = getTableRowContent(elemDt);
-                        break;
-                    case "Producent":
-                        gameProducer = getTableRowContent(elemDt);
-                        break;
-                    case "Liczba nośników":
+            String gameProducent = elements.get(1).text();
+            String gamePublisher = elements.get(2).text();
+            String gameDistributor = elements.get(3).text();
+            String releaseDate = elements.get(4).text();
 
-                        break;
-                    case "Wersja językowa":
+            System.out.println(gameId);
+            System.out.println(gameTitle);
+            System.out.println(gamePrice);
+            System.out.println(gameProducent);
+            System.out.println(gamePublisher);
+            System.out.println(gameDistributor);
+            System.out.println(releaseDate);
 
-                        break;
-                    case "PEGI":
-                        gamePegiUrl = getGamePegiUrl(element);
-                        break;
-                }
-            }
+            System.out.println();
+            System.out.println(            );
 
-            System.out.println(gameId + " -- " + gameImgUrl + " --- " + gameName + " --- " +  gamePrice + " --- " + category + " --- " + gamePegiUrl + " --- " + gameProducer + " ---- " + gamePublisher + " ---- " + gameReleaseDate);
+            System.out.println(elements);
+
 
             game.setId(gameId);
-            game.setName(gameName);
+            game.setName(gameTitle);
 
             gameDetails.setId(gameId);
-            gameDetails.setName(gameName);
+            gameDetails.setName(gameTitle);
             gameDetails.setPosition(position);
             gameDetails.setPrice(gamePrice);
-            gameDetails.setCategory(category);
+            gameDetails.setCategory( gamePage.attr("product-category") );
             gameDetails.setImgUrl(gameImgUrl);
 
+            game.setId((position++) + "");
+            game.setName("test");
+
             games.add(game);
+
         }
 
         return games;
-    }
-
-    private String getGamePegiUrl(Element element) {
-        return "https://www.empik.com" + element.getElementsByClass("pegiCode").first()
-                .getElementsByTag("img").first().attr("src");
-    }
-
-    private Elements getDetailsTableElements(Document gamePage) {
-        return gamePage.getElementsByClass("productDataTable").first()
-                .getElementsByClass("row--text row--text  attributeName");
-    }
-
-    private String getGameCategory(Document gamePage) {
-        return gamePage.getElementsByAttributeValue("itemtype", "http://schema.org/BreadcrumbList").last()
-                .getElementsByAttributeValue("itemprop", "itemListElement").last().text();
-    }
-
-    private String getGamePrice(Document gamePage) {
-        return gamePage.getElementsByClass("ta-price").first().text();
-    }
-
-    private String getGameImgUrl(Document gamePage) {
-        return gamePage.getElementsByAttributeValue("property", "og:image").first().attr("content");
-    }
-
-    private String getGameName(Document gamePage) {
-        return gamePage.getElementsByAttributeValue("property", "og:title").first().attr("content");
-    }
-
-    private String getGameDescription(Document gamePage) {
-        return gamePage.getElementsByClass("productDescription").text();
-    }
-
-    private String getGameId(Document gamePage) {
-        return gamePage.getElementsByClass("js-reco-productlist").first().attr("page-product-id");
-    }
-
-    private String getTableRowContent(String[] elemDt) {
-        return String.join("", Arrays.copyOfRange(elemDt, 1, elemDt.length));
-    }
-
-    private String getPureTextOfTableRow(String row, String text) {
-        return row.substring(row.indexOf(text) + text.length()).trim();
     }
 }
