158c05674afd1d65afc1bdf6e581387397e6d322
brq0 <bartoszturek0@gmail.com>
2018-12-10 21:38:46 +0100

    Merge branch 'master' of C:\studia\4.1\Hurtownie Danych\etlapp with conflicts.

--- src/main/java/com/tmw/etl/etlapp/EtlappApplication.java
+++ src/main/java/com/tmw/etl/etlapp/EtlappApplication.java
@@ -2,8 +2,10 @@ package com.tmw.etl.etlapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.tmw.etl.etlapp")
 public class EtlappApplication {
 
     public static void main(String[] args) {
